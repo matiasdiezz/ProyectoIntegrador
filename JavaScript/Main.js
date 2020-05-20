35705c21f107d5f7f666000c8647d8cb6ac2e402
Matias Diez <mdiez@udesa.edu.ar>
2020-05-20 13:36:18 -0300

    Ultimo cambio

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,34 +1,19 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+window.onload = function () {
+  fetch(
+    "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks"
+  )
+    .then(function (response) {
+      return response.json();
+    })
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+    .then(function (resultado) {
+      //<----------- ESTO ES PARA LOS TRENDING ----------->
+      console.log(resultado);
+    })
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+    .catch(function (error) {
+      console.log("Error: " + error);
+    });
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
-}
+  // NO BORRAR
+};
