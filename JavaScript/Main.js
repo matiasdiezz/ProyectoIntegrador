7a21927b37b9615a3cc609af6fde1c478d5ac64f
Matias Diez <mdiez@udesa.edu.ar>
2020-05-22 17:17:39 -0300

    Html-completo

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,11 +1,13 @@
 window.onload = function () {
-  fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre")
+  var listaCanciones = document.querySelector("listaCanciones");
+  fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
     .then(function (response) {
       return response.json();
     })
 
     .then(function (resultado) {
       console.log(resultado);
+      for (let i = 0; i < resultado.tracks.data.length; i++) {}
     })
 
     .catch(function (error) {
