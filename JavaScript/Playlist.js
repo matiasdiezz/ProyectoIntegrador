162e9fd06bfd319418f7ac02a3621a8d0a2b5ada
felipemartinn <martinf@northlands.edu.ar>
2020-06-12 14:54:27 -0300

    Emprolijamiento y redes

--- JavaScript/Playlist.js
+++ JavaScript/Playlist.js
@@ -1,21 +1,26 @@
 // Playlist
 window.onload = function () {
+  //Recupero el repositorio local
   let recuperoStorage = localStorage.getItem("playlist");
+  //Lo convierto en JSON
   let playlist = JSON.parse(recuperoStorage);
+  //Selcciono la playlist en el HTML
 
   let miPlaylist = document.querySelector("#MiPlaylist");
   console.log(recuperoStorage);
+  //Si la playlist no tiene canciones, te da un mensaje
   if (recuperoStorage == null || recuperoStorage == "[]") {
     playlist = [];
     miPlaylist.innerHTML +=
       "<tr><td> No hay canciones en tu playlist </td></tr>";
     console.log(miPlaylist);
+    //Si lo tiene, te muestra todos los tracks 
   } else {
     playlist.forEach(function (idTrack) {
       mostrarTrack(idTrack);
     });
   }
-
+  //Traigo todas las canciones de la API con un fetch 
   function mostrarTrack(idTrack) {
     fetch(
       "https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/" +
@@ -25,6 +30,7 @@ window.onload = function () {
         return response.json();
       })
       .then(function (respuesta) {
+        //le agrego el HTMl a la playlist
         miPlaylist.innerHTML +=
           "<tr>" +
           '<td class="d-flex justify-content-between align-items-center">' +
@@ -35,6 +41,7 @@ window.onload = function () {
           "</a></td>" +
           "</tr>";
       })
+      //Si hay un error lo muestro en la consola
       .catch(function (errors) {
         console.log(errors);
       });
